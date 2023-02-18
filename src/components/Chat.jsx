d9eb2d3fb3fd0fd86fe9c97005ebbb432c06cf72
commmpotte <kisel.igor.v@yandex.ru>
2023-02-19 01:44:29 +0300

    margin fix

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -5,7 +5,7 @@ import SendMessage from './SendMessage'
 
 function Chat() {
   return (
-    <section className="container wrapper vh-100 ps-md-5 pe-md-5 pt-3">
+    <section className="container wrapper vh-100 ps-md-5 pe-md-5 pt-1">
       <div className="main bg-white mt-4 p-0 ps-md-5 pe-md-5 m-auto">
         <div
           className="p-4 border rounded-5 m-auto"
