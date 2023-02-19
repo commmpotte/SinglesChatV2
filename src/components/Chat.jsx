ffa7d8f9cf43786d0616505dbf86f4bd157a9058
commmpotte <kisel.igor.v@yandex.ru>
2023-02-19 11:23:12 +0300

    height fix

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -6,9 +6,9 @@ import SendMessage from './SendMessage'
 function Chat() {
   return (
     <section className="container wrapper vh-100 ps-md-5 pe-md-5 pt-1">
-      <div className="main bg-white mt-4 p-0 ps-md-5 pe-md-5 m-auto">
+      <div className="main bg-white mt-4 p-0 ps-md-5 pe-md-5 m-auto vh-100">
         <div
-          className="p-4 border rounded-5 m-auto"
+          className="p-4 border rounded-5 m-auto "
           style={{ minWidth: '300px' }}
         >
           <div className="row">
